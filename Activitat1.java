5fc29dc8848b9274705c469618f22bf171202e7a
Elloberas04 <estevellobera@paucasesnovescifp.cat>
2023-09-28 19:46:46 +0200

    Afegim comentaris a l'activitat1

--- Activitat1.java
+++ Activitat1.java
@@ -4,14 +4,18 @@ public class Activitat1
 {
     public static void main(String[] args)
     {
+        //Scanner per poder introduir les dades.
         Scanner sc = new Scanner(System.in);
 
+        //Demanam el primer nombre.
         System.out.print("Primer nombre: ");
         int num1 = sc.nextInt();
 
+        //Demanam el segon nombre.
         System.out.print("Segon nombre: ");
         int num2 = sc.nextInt();
 
+        //Feim els calculs.
         System.out.println("Suma:" + (num1 + num2));
         System.out.println("Resta:" + (num1 - num2));
         System.out.println("Multiplicacio:" + (num1 * num2));
