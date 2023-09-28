7bb8d383b0b7be0ed5546910e31f88ea04c19380
Elloberas04 <estevellobera@paucasesnovescifp.cat>
2023-09-28 19:52:41 +0200

    Afegim comentaris a l'activitat2

--- Activitat2.java
+++ Activitat2.java
@@ -4,22 +4,29 @@ public class Activitat2
 {
     public static void main(String[] args)
     {
+        //Scanner per poder introduir les dades.
         Scanner sc = new Scanner(System.in);
 
+        //Demanam el nom de l'alumne amb l'scanner.
         System.out.print("Nom de l'alumne: ");
         String nom = sc.nextLine();
 
+        //Demanam primera nota.
         System.out.print("NOTA primer trimestre: ");
         int primer = sc.nextInt();
 
+        //Demanam segona nota.
         System.out.print("NOTA segon trimestre: ");
         int segon = sc.nextInt();
 
+        //Demanam tercera nota.
         System.out.print("NOTA tercer trimestre: ");
         int tercer = sc.nextInt();
 
+        //Calculam la nota(només per tres nombres).
         int notamitjana = (primer + segon + tercer) / 3;
 
+        //Mostram el resultat.
         System.out.println("La nota mitjana de l'alumne " + nom + " es de " + notamitjana);
     }
 }
